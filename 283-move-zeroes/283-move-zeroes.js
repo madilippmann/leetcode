d0a1d2d02098b69fbafca786da493a79b3eb91d0
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-05-04 17:48:56 -0700

    Time: 193 ms (11.21%), Space: 46.7 MB (64.58%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = (arr) => {
+  const newArr = arr.filter(num => num !== 0);
+  const numZeros = arr.length - newArr.length;
+  
+  let i = 0;
+  while (i < numZeros) {
+    newArr.push(0);
+    ++i;
+  }
+
+  newArr.forEach((num, i) => arr[i] = num);
+}
