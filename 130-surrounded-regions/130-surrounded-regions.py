5409da61dab0fd9d6b791cf5aa31d5843cd64b2b
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-05-13 20:56:31 -0700

    Time: 283 ms (19.11%), Space: 22.6 MB (13.74%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -0,0 +1,50 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == 'O': 
+                    print('ROWCOL: ', row, col)
+                    tilesToFlip = self.islandTraversal(board, row, col)
+                    print('TILES: ', tilesToFlip)
+                    if tilesToFlip and len(tilesToFlip):
+                        for tileRow, tileCol in tilesToFlip:
+                            board[tileRow][tileCol] = 'X'
+                            
+                        
+                    
+    def islandTraversal(self, board, startRow, startCol):
+        '''
+        return visited set
+        '''
+        dirs = [        [-1, 0], 
+                [ 0,-1],       [ 0,+1],
+                        [+1, 0]]
+        
+        visited = set()
+        visited.add((startRow, startCol))
+        stack = [(startRow, startCol)]
+
+        
+        while len(stack):
+            row, col = stack.pop()
+            
+            if (row <= 0 or col <= 0 or row >= len(board) - 1 or col >= len(board[0]) - 1) :
+                print('false')
+                return False
+            
+            for dRow, dCol in dirs:
+                if not (row+dRow, col+dCol) in visited and board[row+dRow][col+dCol] == 'O':
+                    visited.add((row, col))
+                    stack.append((row + dRow, col + dCol))
+        print('SET: ', visited)
+        return list(visited)
+                
+            
+            
+        
+    
+    
+        
