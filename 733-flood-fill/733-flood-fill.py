fdf6bdc149276e59f6cce7f43563e6eb06e2ec1a
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-05-15 06:43:58 -0700

    Time: 97 ms (54.21%), Space: 14.2 MB (15.01%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,5 +1,30 @@
 class Solution:
+    # Recursive Solution     
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        oldColor = image[sr][sc]
+        
+        def _floodFill(r, c, visited):
+            if (r < 0 or r > len(image) - 1) or (c < 0 or c > len(image[0]) - 1) \
+            or (r, c) in visited \
+            or image[r][c] != oldColor:
+                return
+
+            image[r][c] = newColor
+            visited.add((r, c))
+            
+            _floodFill(r-1, c, visited) # Prev row
+            _floodFill(r+1, c, visited) # Next row
+            _floodFill(r, c-1, visited) # Prev col
+            _floodFill(r, c+1, visited) # Next col
+
+            return image
+        
+        
+        return _floodFill(sr, sc, set())
+    
+    
+    # Iterative Solution
+    def floodFillIterative(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         dirs = [
                         (-1, 0),
                 ( 0,-1),        ( 0,+1),
