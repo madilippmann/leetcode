46bdb44eb61b8ad1aa2d0d1e9a396c6a0dc4d72d
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-05-26 21:12:10 -0700

    Time: 252 ms (77.04%), Space: 21 MB (81.32%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,16 +1,34 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        product = 1
-        zeroCount = 0
+        answer = [1]
         
-        for num in nums:
-            if num != 0: product *= num
-            else: zeroCount += 1
+        for i in range(len(nums) - 1):
+            answer.append(answer[i] * nums[i])
+
+
+        post = 1
+
+        for i in range(len(answer) - 1, 0, -1):
+            post *= nums[i]
+            answer[i-1] *= post
+            
+        return answer
                 
-        for i in range(len(nums)):
-            if zeroCount == 0: nums[i] = product // nums[i]
-            elif zeroCount == 1 and nums[i] == 0: nums[i] = product 
-            else: nums[i] = 0
+
+        
+#     NAIVE SOLUTION
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         product = 1
+#         zeroCount = 0
+        
+#         for num in nums:
+#             if num != 0: product *= num
+#             else: zeroCount += 1
                 
-        return nums
+#         for i in range(len(nums)):
+#             if zeroCount == 0: nums[i] = product // nums[i]
+#             elif zeroCount == 1 and nums[i] == 0: nums[i] = product 
+#             else: nums[i] = 0
+                
+#         return nums
         
