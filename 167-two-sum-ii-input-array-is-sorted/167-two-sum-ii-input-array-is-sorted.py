f040b52b2e04de3854517a0a6c97c7941ca69a38
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-05-28 07:15:00 -0700

    Time: 195 ms (37.36%), Space: 15.1 MB (11.32%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        numsDict = dict()
-        
-        for i, num in enumerate(numbers):
-            if (target - num) in numsDict:
-                return [numsDict[target - num], i+1]
-            else: numsDict[num] = i+1
-                
-            
+        print('----------------------')
+        l, r = 0, len(numbers) - 1
+        print('L: ', l, ' R: ', r)
+        while l < r:
+            if numbers[l] + numbers[r] == target: 
+                print('entered 1')
+                return [l+1, r+1]
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+                print('entered 2 ', l)
+
+            elif numbers[r] == numbers[r - 1] or numbers[l] + numbers[r] > target:
+                r -= 1
+                print('entered 3 ', r)
