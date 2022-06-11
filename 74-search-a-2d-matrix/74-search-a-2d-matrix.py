cd711a7c1492b490b59809cd8c303d9810bc05d6
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-06-11 11:33:54 -0700

    Time: 106 ms (5.41%), Space: 14.4 MB (41.36%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,18 +1,10 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        '''
-        l_row
-        r_col
-        mid_start
-        mid_end
-        
-        '''
         
         l, r = 0, len(matrix) - 1
         
         while l <= r:
             mid = (r + l) // 2
-            print(mid)
 
             if matrix[mid][0] <= target <= matrix[mid][-1]:
                 # do another round of binary search over row and return true if found, false if not found
@@ -32,11 +24,9 @@ class Solution:
 
                 
             elif target <= matrix[mid][0]:
-                print('entered 1')
                 r = mid - 1
             
             elif target > matrix[mid][-1]:
-                print('entered 2')
                 l = mid + 1
             
             else: 
