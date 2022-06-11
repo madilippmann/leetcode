7a3c7041b3b8f1794089e9aa125dc5315540da86
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-06-11 06:20:13 -0700

    Time: 2728 ms (5.20%), Space: 49.8 MB (5.90%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -17,18 +17,14 @@ class Solution:
             i += 1
             j -= 1
 
-        # print(left_sums, right_sums)
-        
         min_ops = float('inf')
         
         if x in left_sums:
-            # print('ENTERED: ', left_sums[x])
             min_ops = min(left_sums[x], min_ops)
         if x in right_sums:
             min_ops = min(right_sums[x], min_ops)
         for k in left_sums:
             if x - k in right_sums and left_sums[k] + right_sums[x-k] < len(nums):
-                # print('HAS IT! ', k, x - k)
                 min_ops = min(left_sums[k] + (right_sums[x - k]), min_ops)
         
         return min_ops if not min_ops == float('inf') else -1
