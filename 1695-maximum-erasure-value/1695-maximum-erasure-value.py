36829f9336bc490866807819d5f9c461033f6f9d
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-06-12 10:54:03 -0700

    Time: 1375 ms (82.50%), Space: 27.9 MB (40.92%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -7,33 +7,22 @@ class Solution:
         
         '''
         l, r = 0, 1
-        local_max = global_max = nums[l]
+        global_max = nums[l]
         counts = {nums[0]: 0}
         
         sums = [nums[0]]
         
         for i in range(1, len(nums)):
             sums.append(sums[i-1] + nums[i])
-
         
         while l < r and r < len(nums):
             if nums[r] in counts and counts[nums[r]] >= l:
                 l = counts[nums[r]] + 1
-                # print('before: ', local_max)
-                
-                # local_max -= sums[l - 1]
-                # print('after: ', local_max)
-
-            # else:
-                # local_max += nums[r]
-                # global_max = max(global_max, local_max)
 
             counts[nums[r]] = r
             global_max = max(global_max, sums[r] - sums[l - 1] if l > 0 else sums[r])            
-            # print(nums[r], local_max, counts, f"left: {l}  right: {r}")
             r += 1
             
-        print(local_max)
         return global_max
         
         
