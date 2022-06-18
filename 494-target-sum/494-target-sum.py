5e8857897b1474e05dcdfede7f5eec80d2281adc
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-06-17 21:11:49 -0700

    Time: 638 ms (20.81%), Space: 36.1 MB (13.30%) - LeetHub

---
+++ 494-target-sum/494-target-sum.py
@@ -0,0 +1,45 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+        
+        def backtrack(i, total):
+            if i == len(nums):
+                if total == target:
+                    return 1
+                else:
+                    return 0
+            if (i, total) in memo:
+                return memo[(i, total)]
+            
+            memo[(i, total)] = backtrack(i+1, total + nums[i]) + backtrack(i+1, total - nums[i])
+            return memo[(i, total)]
+        res = backtrack(0, 0)
+        print(memo)
+        
+        return res
+        
+        
+        
+        
+        
+        
+        
+        """
+        BRUTE FORCE
+        memo = [-nums[0], nums[0]] 
+
+        for num in nums[1:]:
+            i, length = 0, len(memo)
+            while i < length:
+                memo.append(memo[i] - num)
+                memo[i] += num
+                i += 1
+        
+        matches = 0
+        
+        for entry in memo:
+            if entry == target:
+                matches += 1
+
+        return matches
+        """
