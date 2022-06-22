7e12d1ff607d9236229c0544c9c4c20f1b6f0d44
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-06-21 18:54:49 -0700

    Time: 78 ms (14.56%), Space: 14.1 MB (21.63%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def backtrack(path, available_nums):
+            print(path, available_nums)
+            if len(path) == len(nums):
+                res.append(path)
+                return 
+            
+            for i, num in enumerate(available_nums):
+                backtrack(path + [num], available_nums[0:i] + available_nums[i+1:])
+            
+        
+            
+            
+            
+        for i, num in enumerate(nums):
+            backtrack([num], nums[0:i] + nums[i+1:])  
+        
+        return res
