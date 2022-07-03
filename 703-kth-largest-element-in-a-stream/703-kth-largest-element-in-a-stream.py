17738f8f78a19b0d883aabc8bcaca7a455092aa8
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-07-03 08:35:51 -0700

    Time: 185 ms (26.91%), Space: 18.4 MB (24.72%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
@@ -1,30 +1,51 @@
+import heapq
 class KthLargest:
-
+#    Solution using heapq module
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.nums = sorted(nums)
+        self.nums = nums
+        heapq.heapify(self.nums)
+        
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
 
         
     def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
         
-        l, r = 0, len(self.nums) 
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
         
-        while l <= r:
-            mid = (r + l) // 2
+        return self.nums[0]
+        
+    
 
-            if mid == len(self.nums):
-                mid += 1
-                break
-            elif val == self.nums[mid] or (mid - 1 >= 0  and self.nums[mid - 1] < val < self.nums[mid]):
-                break
-            elif val > self.nums[mid]:
-                l = mid + 1
-            else:
-                r = mid - 1
+# Inefficient solution not utilizing heap data structure
+#     def __init__(self, k: int, nums: List[int]):
+#         self.k = k
+#         self.nums = sorted(nums)
+
         
-        self.nums = self.nums[:mid] + [val] + self.nums[mid:]
+#     def add(self, val: int) -> int:
         
-        return self.nums[-self.k]
+#         l, r = 0, len(self.nums) 
+        
+#         while l <= r:
+#             mid = (r + l) // 2
+
+#             if mid == len(self.nums):
+#                 mid += 1
+#                 break
+#             elif val == self.nums[mid] or (mid - 1 >= 0  and self.nums[mid - 1] < val < self.nums[mid]):
+#                 break
+#             elif val > self.nums[mid]:
+#                 l = mid + 1
+#             else:
+#                 r = mid - 1
+        
+#         self.nums = self.nums[:mid] + [val] + self.nums[mid:]
+        
+#         return self.nums[-self.k]
 
 
 # Your KthLargest object will be instantiated and called as such:
