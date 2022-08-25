b00dbcdeb8445861542db636fba96174408afd40
Madi Lippmann <66145354+madilippmann@users.noreply.github.com>
2022-08-25 07:38:14 -0700

    Time: 233 ms (5.00%), Space: 14.2 MB (20.43%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        
+        magazineDict = dict()
+        
+        for c in magazine:
+            magazineDict[c] = magazineDict.get(c, 0) + 1
+        
+        # print(magazineDict)
+        for c in ransomNote:
+            if not c in magazineDict or magazineDict[c] == 0:
+                return False
+            else:
+                magazineDict[c] -= 1
+        
+        return True
